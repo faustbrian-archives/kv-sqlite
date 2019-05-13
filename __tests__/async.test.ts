5c0611309d3c62872247022e8c9c82124337b084
Brian Faust <faustbrian@users.noreply.github.com>
2019-05-13 04:57:44 +0200

    add ruby-style initializer to allow use of async (StoreSync.new, StoreAsync.new) (#2)

--- __tests__/async.test.ts
+++ __tests__/async.test.ts
@@ -2,10 +2,10 @@ import { complianceTestsAsync } from "@keeveestore/test-suite";
 import tempy from "tempy";
 import { StoreAsync } from "../src/async";
 
-complianceTestsAsync(new StoreAsync<string, number>({ connection: tempy.file(), type: "INTEGER" }), {
-	"1": 1,
-	"2": 2,
-	"3": 3,
-	"4": 4,
-	"5": 5,
+complianceTestsAsync(() => StoreAsync.new<string, number>({ connection: tempy.file(), type: "INTEGER" }), {
+	1: 1,
+	2: 2,
+	3: 3,
+	4: 4,
+	5: 5,
 });
